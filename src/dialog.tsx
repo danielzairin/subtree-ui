0138184219eea447a335181641ebd961bb47453a
Daniel Zairin <danielzairink@gmail.com>
2024-09-14 19:04:36 +0800

    initial commit

---
+++ src/dialog.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+export type DialogProps = {
+  children?: React.ReactNode;
+};
+
+export type DialogRef = {
+  show: () => void;
+  showModal: () => void;
+  close: () => void;
+};
+
+export const Dialog = forwardRef<DialogRef, DialogProps>((props, ref) => {
+  const { children } = props;
+  const dialogElementRef = useRef<HTMLDialogElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    show: () => dialogElementRef.current?.show(),
+    showModal: () => dialogElementRef.current?.showModal(),
+    close: () => dialogElementRef.current?.close(),
+  }));
+
+  return <dialog ref={dialogElementRef}>{children}</dialog>;
+});
+
+export function useDialogRef() {
+  return useRef<DialogRef>(null);
+}
